e2bae9b5406c8142de106675c628ec463b945137
Todor Yadkov <toni_12@abv.bg>
2023-01-18 16:19:37 +0200

    Exercise

---
+++ JS-Applications-2023/Asynchronous Programming - Lab/01.XHR/scripts/app.js
@@ -0,0 +1,25 @@
+async function loadRepos() {
+   const output = document.getElementById('res');
+   try {
+      const response = await fetch('https://api.github.com/users/testnakov/repos');
+
+      if (response.ok === false) {
+         throw new Error(`Houston, we have a problem: ${response.statusText}`);
+      }
+
+      output.textContent = JSON.stringify(await response.json());
+
+   } catch (err) {
+     output.alert(err);
+   }
+}
+
+
+/* // Solution using fetch with then -> catch construct
+function loadRepos() {
+   const output = document.getElementById('res');
+   fetch('https://api.github.com/users/testnakov/repos')
+      .then(response => response.json())
+      .then(response => output.textContent = JSON.stringify(response))
+      .catch(reject => output.alert(`Houston, we have a problem: ${reject.message}`));
+} */
