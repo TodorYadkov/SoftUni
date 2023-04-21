d981079fbc7f8b8302262b970bb8c83003623482
Todor Yadkov <toni_12@abv.bg>
2023-04-21 10:15:44 +0300

    Exercise

---
+++ JS-Back-End-2023/Exam Preparation/Book_Talk/config/routesConfig.js
@@ -0,0 +1,24 @@
+const { catalogController } = require('../controllers/catalogController.js');
+const { controller404 } = require('../controllers/controller404.js');
+const { createController } = require('../controllers/createController.js');
+const { deleteController } = require('../controllers/deleteController.js');
+const { detailsController } = require('../controllers/detailsController.js');
+const { editController } = require('../controllers/editController.js');
+const { homeController } = require('../controllers/homeController.js');
+const { profileController } = require('../controllers/profileController.js');
+const { userController } = require('../controllers/userController.js');
+const { isLogged } = require('../middleware/guards.js');
+
+module.exports = (app) => {
+
+    app.use(homeController);
+    app.use('/user', userController);
+    app.use('/create', isLogged, createController);
+    app.use('/catalog', catalogController);
+    app.use('/details', detailsController);
+    app.use('/delete', isLogged, deleteController);
+    app.use('/edit', isLogged, editController);
+    app.use('/profile', isLogged, profileController);
+
+    app.all('*', controller404);
+};
