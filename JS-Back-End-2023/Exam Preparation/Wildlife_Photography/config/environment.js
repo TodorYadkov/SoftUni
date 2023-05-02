a53322ffc8a7b97e28df743deb320b1f83ca5107
Todor Yadkov <toni_12@abv.bg>
2023-05-02 13:54:40 +0300

    Exercise

---
+++ JS-Back-End-2023/Exam Preparation/Wildlife_Photography/config/environment.js
@@ -0,0 +1,15 @@
+const nameDB = 'wildlifePhotography';
+const CONNECTION_STR = `mongodb://127.0.0.1:27017/${nameDB}`;
+const serverPort = 3000;
+const jwtSecret = 'DFHDFaswerq@#4ASDFvssd345hioA!@#$ASDF5ASF36asdfas234';
+const userCookieName = 'userData';
+const roundsBcrypt = 10;
+
+module.exports = {
+    serverPort,
+    jwtSecret,
+    roundsBcrypt,
+    CONNECTION_STR,
+    nameDB,
+    userCookieName,
+};
