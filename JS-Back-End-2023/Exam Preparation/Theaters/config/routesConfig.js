26975ad4389adbe7737a5d3be4bd08768c9fc5be
Todor Yadkov <toni_12@abv.bg>
2023-05-09 19:24:35 +0300

    Exercise

---
+++ JS-Back-End-2023/Exam Preparation/Theaters/config/routesConfig.js
@@ -0,0 +1,17 @@
+const { createController } = require('../controllers/createController');
+const { deleteController } = require('../controllers/deleteController');
+const { detailsController } = require('../controllers/detailsController');
+const { editController } = require('../controllers/editController');
+const { homeController } = require('../controllers/homeController');
+const { userController } = require('../controllers/userController');
+const { isLogged } = require('../middleware/guards');
+
+module.exports = (app) => {
+    app.use(homeController);
+    app.use('/user', userController);
+    app.use('/create', isLogged, createController);
+    app.use('/details', isLogged, detailsController);
+    app.use('/edit', isLogged, editController);
+    app.use('/delete', isLogged, deleteController);
+    
+};
