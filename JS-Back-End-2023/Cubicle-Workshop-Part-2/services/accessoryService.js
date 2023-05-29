f402adb18e6f3476cbeec47e9bfbf73a2d1dbab0
Todor Yadkov <todor.yadkov@gmail.com>
2023-05-29 21:56:29 +0300

    Exercise

--- JS-Back-End-2023/Cubicle-Workshop-Part-2/services/accessoryService.js
+++ JS-Back-End-2023/Cubicle-Workshop-Part-2/services/accessoryService.js
@@ -5,8 +5,7 @@ const { getCubeById } = require('./cubeServices.js');
 async function createAccessory(accessoryData) {
     return Accessory.create(accessoryData);
 }
-// When I create the function like this, lean() works called on the returned result, 
-// and when it's a regular function lean() works only inside the function
+
 const getOnlyUnused = (ids) => Accessory.find({ _id: { $nin: ids } });
 
 async function getAllAccessories() {
