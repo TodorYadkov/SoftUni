b250288a06f63bda23aa003464030655bfc76ede
Todor Yadkov <toni_12@abv.bg>
2023-06-19 11:09:37 +0300

    Exercise

---
+++ JS-Back-End-2023/Exam Preparation/Friendly_World/config/environment.js
@@ -0,0 +1,15 @@
+const nameDB = 'friendlyWorld';
+const CONNECTION_STR = `mongodb://127.0.0.1:27017/${nameDB}`;
+const serverPort = 3000;
+const jwtSecret = '553ba1a62daee975a374c0b7d0d96caefeb1ba92';
+const userCookieName = 'userData';
+const roundsBcrypt = 10;
+
+module.exports = {
+    serverPort,
+    jwtSecret,
+    roundsBcrypt,
+    CONNECTION_STR,
+    nameDB,
+    userCookieName,
+};
