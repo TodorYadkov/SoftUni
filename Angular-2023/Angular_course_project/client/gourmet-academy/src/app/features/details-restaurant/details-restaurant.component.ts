1fe93b74b70b978ee97d24037343bb672397f1ca
Todor Yadkov <toni_12@abv.bg>
2023-07-06 01:39:41 +0300

    Update details-restaurant.component.ts

--- Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/details-restaurant/details-restaurant.component.ts
+++ Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/details-restaurant/details-restaurant.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription, mergeMap, tap } from 'rxjs';
+import { DataService } from 'src/app/core/services/data/data.service';
+import { IComment } from 'src/app/models/comment.interfaces';
+import { IRestaurant } from 'src/app/models/restaurant.interfaces';
 
 @Component({
   selector: 'app-details-restaurant',
@@ -6,17 +12,47 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrls: ['./details-restaurant.component.css']
 })
 export class DetailsRestaurantComponent implements OnInit, OnDestroy {
+  subscription!: Subscription;
+  restaurantDetails!: IRestaurant;
+  restaurantId!: string;
+  isLoading: boolean = false;
+  errorMsgFromServer!: string;
+  allComments: IComment[] = [];
 
+  constructor(
+    private title: Title,
+    private dataService: DataService,
+    private activeRoute: ActivatedRoute,
+  ) { }
 
-  constructor() { }
-
-  
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.title.setTitle('Детайли');
+
+    this.restaurantId = this.activeRoute.snapshot.params['restaurantId']; // Get restaurant Id
+    this.isLoading = true; // Show spinner
+    // Get restaurant details
+    this.subscription = this.dataService.getRestaurantById(this.restaurantId)
+      .pipe(
+        mergeMap(restaurant => {
+          this.restaurantDetails = restaurant;
+          return this.dataService.getAllCommentsRestaurant(restaurant._id)
+            .pipe(
+              mergeMap(comments => this.allComments = comments.slice(-5).reverse()) // Get last added five comment
+            )
+        })
+      ).subscribe({
+        next: (data) => this.isLoading = false,
+        error: (error) => {
+          this.errorMsgFromServer = error.error.message.join('\n');
+          this.isLoading = false;
+        }
+      });
   }
 
 
   ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
+    if (this.subscription !== undefined) {
+      this.subscription.unsubscribe();
+    }
   }
 }
