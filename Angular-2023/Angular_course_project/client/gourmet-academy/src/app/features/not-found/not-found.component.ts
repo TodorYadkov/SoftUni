e37120249474d809f82eb0bf63e619377edcba77
Todor Yadkov <toni_12@abv.bg>
2023-07-06 01:39:29 +0300

    Update not-found.component.ts

--- Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/not-found/not-found.component.ts
+++ Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/not-found/not-found.component.ts
@@ -1,21 +1,30 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
   pageX: number;
   pageY: number;
   mouseY: number = 0;
   mouseX: number = 0;
 
-  constructor() {
+  constructor(private title: Title) {
     this.pageX = document.documentElement.clientWidth;
     this.pageY = document.documentElement.clientHeight;
+
   }
 
+
+  ngOnInit(): void {
+    this.title.setTitle('404');
+
+  }
+
+  // Code is from https://codepen.io/diogo_ml_gomes/pen/PyWdLb
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
     // verticalAxis
