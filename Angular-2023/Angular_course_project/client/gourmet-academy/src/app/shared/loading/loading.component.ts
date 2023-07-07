296a39effda74bbc9a92c0e83dd92956d281763a
Todor Yadkov <toni_12@abv.bg>
2023-07-07 08:15:35 +0300

    Update loading.component.ts

--- Angular-2023/Angular_course_project/client/gourmet-academy/src/app/shared/loading/loading.component.ts
+++ Angular-2023/Angular_course_project/client/gourmet-academy/src/app/shared/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./loading.component.css']
 })
 export class LoadingComponent {
-
+  @Input() isLoading!: boolean;
 }
