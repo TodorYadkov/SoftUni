9a1a91a22da83f1d7bf106a5c738fd8780b1e8d8
Todor Yadkov <toni_12@abv.bg>
2023-07-07 08:16:52 +0300

    Update app-routing.module.ts

--- Angular-2023/Angular_course_project/client/gourmet-academy/src/app/app-routing.module.ts
+++ Angular-2023/Angular_course_project/client/gourmet-academy/src/app/app-routing.module.ts
@@ -8,22 +8,18 @@ import { ProfileComponent } from './features/profile/profile.component';
 import { AboutUsComponent } from './features/about-us/about-us.component';
 import { DetailsRestaurantComponent } from './features/details-restaurant/details-restaurant.component';
 import { NotFoundComponent } from './features/not-found/not-found.component';
-import { AddProductComponent } from './features/add-product/add-product.component';
-import { EditProductComponent } from './features/edit-product/edit-product.component';
 import { onlyForGuestGuard } from './core/guards/only-for-guest.guard';
 import { onlyForLoggedInGuard } from './core/guards/only-for-logged-in.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'register', canActivate: [onlyForGuestGuard], component: RegisterComponent },
   { path: 'login', canActivate: [onlyForGuestGuard], component: LoginComponent },
+  { path: 'register', canActivate: [onlyForGuestGuard], component: RegisterComponent },
   { path: 'add-restaurants', canActivate: [onlyForLoggedInGuard], component: AddRestaurantComponent },
   { path: 'profile', canActivate: [onlyForLoggedInGuard], component: ProfileComponent },
-  { path: 'about-us', component: AboutUsComponent },
-  // {path: 'orders/:restaurant._id', component: }, TODO COMPONENT WHERE TO USER TO BUY
   { path: 'details/:restaurantId', canActivate: [onlyForLoggedInGuard], component: DetailsRestaurantComponent },
-  { path: 'products/:productId', canActivate: [onlyForLoggedInGuard], component: AddProductComponent },
-  { path: 'products/edit/:productId', canActivate: [onlyForLoggedInGuard], component: EditProductComponent },
+  // {path: 'orders/:restaurant._id', component: }, TODO COMPONENT WHERE TO USER TO BUY
+  { path: 'about-us', component: AboutUsComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
