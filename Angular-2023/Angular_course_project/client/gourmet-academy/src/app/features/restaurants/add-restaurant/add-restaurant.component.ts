bd7f4f4aaef374d383a146716533e95badff5c12
Todor Yadkov <toni_12@abv.bg>
2023-07-09 10:14:15 +0300

    Update add-restaurant.component.ts

--- Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/restaurants/add-restaurant/add-restaurant.component.ts
+++ Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/restaurants/add-restaurant/add-restaurant.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { DataService } from 'src/app/core/services/data/data.service';
 import { IRestaurant } from 'src/app/models/restaurant.interfaces';
+import { ValidateRestaurantService } from '../validate-restaurant.service';
 
 @Component({
   selector: 'app-add-restaurant',
@@ -12,53 +13,33 @@ import { IRestaurant } from 'src/app/models/restaurant.interfaces';
   styleUrls: ['./add-restaurant.component.css']
 })
 export class AddRestaurantComponent implements OnInit, OnDestroy {
-  retaurant!: IRestaurant;
+
   subscription!: Subscription;
   errorMsgFromServer!: string;
   isLoading: boolean = false;
 
   constructor(
-    private dataService: DataService,
-    private router: Router,
     private title: Title,
+    private router: Router,
+    private dataService: DataService,
+    private validateRestaurant: ValidateRestaurantService
   ) { }
 
   ngOnInit(): void {
-        this.title.setTitle('Ресторант');
+    this.title.setTitle('Ресторант');
   }
 
   createRestaurant(formData: NgForm) {
     const userInput: IRestaurant = formData.value;
-
     // Validate user input
-    if (Object.values(formData.value).some(v => v === '')) {
-      this.errorMsgFromServer = 'Всички полета са задължителни';
-
-    } else if (userInput.name.length < 2 || userInput.name.length > 50) {
-      this.errorMsgFromServer = 'Името трябва да бъде между 2 и 50 символа';
-
-    } else if (userInput.location.length < 2 || userInput.location.length > 50) {
-      this.errorMsgFromServer = 'Градът трябва да бъде между 2 и 50 символа';
-
-    } else if (userInput.address.length < 5 || userInput.address.length > 100) {
-      this.errorMsgFromServer = 'Адресът трябва да бъде между 5 и 100 символа';
-
-    } else if (/^\+\d{3}\d{3}\d{3}\d{3}$/.test(userInput.phone) === false) {
-      this.errorMsgFromServer = 'Моля ползвайте следният формат +359111222333'
-
-    } else if (userInput.cuisine.length < 5 || userInput.cuisine.length > 40) {
-      this.errorMsgFromServer = 'Предлагана кухня трябва да бъде между 5 и 40 символа';
-
-    } else if (userInput.description.length < 5 || userInput.description.length > 200) {
-      this.errorMsgFromServer = 'Описанието трябва да бъде между 5 и 200 символа';
-
-    } else if (/^https?:\/\/[^ ]+$/.test(userInput.image) === false) {
-      this.errorMsgFromServer = ' Снимката трябва да бъде линк започващ с http:// или https://';
+    const restaurantCheck = this.validateRestaurant.validate(userInput);
+    if (restaurantCheck.hasError) {
+      this.errorMsgFromServer = restaurantCheck.error;
 
     } else {
-      
+
       this.isLoading = true;
-      this.subscription = this.dataService.createRestaurant(userInput)
+      this.subscription = this.dataService.createRestaurant(restaurantCheck.verifiedInput)
         .subscribe({
           next: (data) => {
             formData.reset();
@@ -78,5 +59,5 @@ export class AddRestaurantComponent implements OnInit, OnDestroy {
       this.subscription.unsubscribe();
     }
   }
-
+  
 }
