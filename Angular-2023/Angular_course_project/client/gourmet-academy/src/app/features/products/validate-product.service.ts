61fb222d757e5dd03e4502b28898429d65effb28
Todor Yadkov <toni_12@abv.bg>
2023-07-09 10:14:17 +0300

    Update validate-product.service.ts

--- Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/products/validate-product.service.ts
+++ Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/products/validate-product.service.ts
@@ -8,31 +8,43 @@ export class ValidateProductService {
 
   constructor() { }
 
-  validate(productData: IProduct): { hasError: boolean, error: string } {
-    const result: { hasError: boolean, error: string } = {
+  validate(productData: IProduct): { hasError: boolean, error: string, verifiedInput: IProduct } {
+    const productDataTrimmed = Object
+      .entries(productData)
+      .reduce((acc, currentValue) => { // Trim user input
+        currentValue[1] = typeof currentValue[1] === 'string'
+          ? currentValue[1].trim()
+          : currentValue[1]; // Check if the current value is string
+
+        return Object.assign({ [currentValue[0]]: currentValue[1] }, acc); // Return object with assign current key values pair
+      }, {}) as IProduct; // Saved like IProduct
+
+    // Create an object to return checked productData
+    const result: { hasError: boolean, error: string, verifiedInput: IProduct } = {
       hasError: false,
       error: '',
+      verifiedInput: productDataTrimmed
     };
-
-    if (Object.values(productData).some(v => v === '')) {
+    // Validate User input for product data
+    if (Object.values(result.verifiedInput).some(v => v === '')) {
       result.error = 'Всички полета са задължителни';
 
-    } else if (productData.name.length < 3 || productData.name.length > 100) {
+    } else if (result.verifiedInput.name.length < 3 || result.verifiedInput.name.length > 100) {
       result.error = 'Името на продукта трябва да бъде между 3 и 100 символа';
 
-    } else if (productData.weight.length < 2 || productData.weight.length > 10) {
+    } else if (result.verifiedInput.weight.length < 2 || result.verifiedInput.weight.length > 10) {
       result.error = 'Мярката трябва да бъде между 2 и 10 символа';
 
-    } else if (isNaN(Number(productData.price)) || Number(productData.price) < 0) {
+    } else if (isNaN(Number(result.verifiedInput.price)) || Number(result.verifiedInput.price) < 0) {
       result.error = 'Моля въведете коректна цена'
 
-    } else if (productData.group.length < 3 || productData.group.length > 20) {
+    } else if (result.verifiedInput.group.length < 3 || result.verifiedInput.group.length > 20) {
       result.error = 'Групата трябва да бъде между 3 и 20 символа';
 
-    } else if (/^https?:\/\/[^ ]+$/.test(productData.image) === false) {
+    } else if (/^https?:\/\/[^ ]+$/.test(result.verifiedInput.image) === false) {
       result.error = 'Снимката трябва да бъде линк започващ с http:// или https://'
     }
-    console.log(productData)
+
     if (result.error) {
       result.hasError = true;
     }
