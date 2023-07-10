3294afacbf650bcf876c23cea4d02f8966d7b34f
Todor Yadkov <toni_12@abv.bg>
2023-07-10 10:01:20 +0300

    Update edit-restaurant.component.ts

--- Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/restaurants/edit-restaurant/edit-restaurant.component.ts
+++ Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/restaurants/edit-restaurant/edit-restaurant.component.ts
@@ -1,26 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { IRestaurant } from 'src/app/models/restaurant.interfaces';
+import { ValidateRestaurantService } from '../validate-restaurant.service';
+import { DataService } from 'src/app/core/services/data/data.service';
 
 @Component({
   selector: 'app-edit-restaurant',
   templateUrl: './edit-restaurant.component.html',
   styleUrls: ['./edit-restaurant.component.css']
 })
-export class EditRestaurantComponent {
-  retaurant!: IRestaurant;
+export class EditRestaurantComponent implements OnDestroy {
 
-  editRestaurant(formData: NgForm) {
-    console.log(formData.value);
+  @Input() restaurantDetails!: IRestaurant;
+  @Output() restaurantUpdated = new EventEmitter;
+
+  successMessage!: string;
+  errorMsgFromServer!: string;
+  isLoading: boolean = false;
+  subscription!: Subscription;
+  imageUrl!: string;
+
+  constructor(
+    private dataService: DataService,
+    private validateRestaurant: ValidateRestaurantService
+  ) { }
+
+  onEditRestaurant(formData: NgForm) {
+    const restaurantData: IRestaurant = formData.value;
+    // Validate user input
+    const restaurantCheck = this.validateRestaurant.validate(restaurantData);
+    if (restaurantCheck.hasError) {
+      this.errorMsgFromServer = restaurantCheck.error;
+
+    } else {
+      this.isLoading = true;
+      this.subscription = this.dataService
+        .updateRestaurant(this.restaurantDetails._id, restaurantCheck.verifiedInput)
+        .subscribe({
+          next: (data) => {
+            this.isLoading = false;
+            this.restaurantDetails = data;
+            this.successMessage = `Успешно редактирахте ${data.name}`;
+            this.restaurantUpdated.emit(); // Refresh data on details page
+          },
+          error: (error) => {
+            this.isLoading = false;
+            this.errorMsgFromServer = error.error.message.join('\n');
+          }
+        });
+    }
   }
-}
 
-// Only to create html move in Models
-interface IRestaurant {
-  name: string;
-  location: string;
-  phone: string;
-  address: string;
-  cuisine: string;
-  description: string;
-  image: string;
+  // Check if the current html path is correct
+  validateImagePath(imagePath: string) {
+    this.imageUrl = this.validateRestaurant.validateImagePath(imagePath);
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
