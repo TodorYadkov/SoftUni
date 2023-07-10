124e8018dbf43edb55b68b0dcfea10104750c983
Todor Yadkov <toni_12@abv.bg>
2023-07-10 10:02:35 +0300

    Update edit-product.component.ts

--- Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/products/edit-product/edit-product.component.ts
+++ Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/products/edit-product/edit-product.component.ts
@@ -63,7 +63,7 @@ export class EditProductComponent implements OnDestroy {
   }
 
   ngOnDestroy(): void {
-    if (this.subscription !== undefined) {
+    if (this.subscription) {
       this.subscription.unsubscribe();
     }
   }
