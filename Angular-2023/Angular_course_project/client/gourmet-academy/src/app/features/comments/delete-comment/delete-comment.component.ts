60bdbd5dfd41fa955a03edc0777e69523f42b391
Todor Yadkov <toni_12@abv.bg>
2023-07-12 08:20:56 +0300

    Update delete-comment.component.ts

--- Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/comments/delete-comment/delete-comment.component.ts
+++ Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/comments/delete-comment/delete-comment.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DataService } from 'src/app/core/services/data/data.service';
 import { IComment } from 'src/app/models/comment.interfaces';
 
 @Component({
@@ -6,7 +8,45 @@ import { IComment } from 'src/app/models/comment.interfaces';
   templateUrl: './delete-comment.component.html',
   styleUrls: ['./delete-comment.component.css']
 })
-export class DeleteCommentComponent {
+export class DeleteCommentComponent implements OnDestroy {
 
   @Input() commentDetails!: IComment;
+  @Output() updateCommentList = new EventEmitter;
+
+  successMessage!: string;
+  errorMsgFromServer!: string;
+  isLoading: boolean = false;
+  subscription!: Subscription;
+  disableBtnDelete: boolean = false;
+
+  constructor(private dataService: DataService) { }
+
+  // Delete comment
+  deleteComment(commentId: string): void {
+    this.isLoading = true;
+    this.subscription = this.dataService
+      .deleteComment(commentId)
+      .subscribe({
+        next: (data) => {
+          this.isLoading = false;
+          this.disableBtnDelete = true;
+          this.successMessage = `Успешно изтрихте: ${data.deletedComment.comment}`;
+        },
+        error: (error) => {
+          this.isLoading = false;
+          this.errorMsgFromServer = error.error.message.join('\n');
+        }
+      })
+  }
+
+  // Update comment list on add-comments component
+  onCloseModal(): void {
+    this.updateCommentList.emit();
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
