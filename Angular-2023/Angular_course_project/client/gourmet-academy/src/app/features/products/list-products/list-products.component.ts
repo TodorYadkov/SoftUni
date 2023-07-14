e3d6f8a936b445d7c2e689931ef6685dfba6bc2e
Todor Yadkov <toni_12@abv.bg>
2023-07-14 08:53:45 +0300

    Update list-products.component.ts

--- Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/products/list-products/list-products.component.ts
+++ Angular-2023/Angular_course_project/client/gourmet-academy/src/app/features/products/list-products/list-products.component.ts
@@ -21,7 +21,7 @@ export class ListProductsComponent implements OnInit, OnDestroy {
   // Use this for pagination and search
   currentProducts!: IProduct[]; // Current products on the page
   isFoundResult: boolean = false; // Use to show and hide a message if there are any products found or not
-  productsPerPage = 20; // Number of products to display per page
+  productsPerPage = 10; // Number of products to display per page
   currentPage = 1; // Current page
   totalPages = 0; // Total number of pages
   pages: number[] = []; // Array of page numbers for rendering page numbers
@@ -64,33 +64,47 @@ export class ListProductsComponent implements OnInit, OnDestroy {
   changeProductsToShow(newValue: string): void {
     if (newValue) {
       const valueToNumber = Number(newValue);
+      // Check if the value is -1 get all products, else current number of products
       this.productsPerPage = valueToNumber === -1 ? this.allProducts.length : valueToNumber;
-      // Set pagination options
-      this.calculateTotalPages(this.allProducts.length);
-      this.updatePageRange();
-      this.productToDisplay();
+    } else {
+      // Set default value when the new value is empty string
+      this.productsPerPage = 10;
     }
+
+    // Set pagination options
+    this.calculateTotalPages(this.allProducts.length);
+    this.updatePageRange();
+    this.productToDisplay();
+    this.goToPage(1);
   }
 
   // Sort product by criteria
   sortProductsBy(criteria: string): void {
-    this.currentProducts = this.allProducts.sort((a, b) => {
-      if (criteria === 'name') {
-        // Sort by name
-        return a.name.localeCompare(b.name);
-      } else if (criteria === 'group') {
-        // Sort by group
-        return a.group.localeCompare(b.group);
-      } else if (criteria === 'priceASC') {
-        // Sort by price ASC
-        return a.price - b.price;
-      } else if (criteria === 'priceDSC') {
-        // Sort by price DSC
-        return b.price - a.price;
-      }
+    if (criteria === '') {
+      // Default sort
+      this.currentProducts = [...this.allProducts];
+      
+    } else {
+      // Sort by criteria
+      const productToSort = [...this.allProducts]; // Get s new reference to all products
+      this.currentProducts = productToSort.sort((a, b) => {
+        if (criteria === 'name') {
+          // Sort by name
+          return a.name.localeCompare(b.name);
+        } else if (criteria === 'group') {
+          // Sort by group
+          return a.group.localeCompare(b.group);
+        } else if (criteria === 'priceASC') {
+          // Sort by price ASC
+          return a.price - b.price;
+        } else if (criteria === 'priceDSC') {
+          // Sort by price DSC
+          return b.price - a.price;
+        }
 
-      return 0;
-    }).slice();
+        return 0;
+      });
+    }
   }
 
   // Get all products for the current restaurant
