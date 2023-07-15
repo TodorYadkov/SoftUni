4b9ee55a13f03627e1d58f07100549c08a60d17d
Todor Yadkov <toni_12@abv.bg>
2023-07-15 09:34:53 +0300

    Update only-for-logged-in.guard.ts

--- Angular-2023/Angular_course_project/client/gourmet-academy/src/app/core/guards/only-for-logged-in.guard.ts
+++ Angular-2023/Angular_course_project/client/gourmet-academy/src/app/core/guards/only-for-logged-in.guard.ts
@@ -1,23 +1,17 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree, } from '@angular/router';
-import { Observable } from 'rxjs';
-
-import { constants } from '../environments/constants';
+import { ManagerSessionService } from '../services/users/manager-session.service';
 
 @Injectable({ providedIn: 'root' })
 export class onlyForLoggedInGuard implements CanActivate {
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private managerSession: ManagerSessionService
+  ) { }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
-    | boolean
-    | UrlTree
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree> {
-    const token = localStorage.getItem(constants.userTokenName);
-    if (token) {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
+
+    if (this.managerSession.hasUser) {
 
       return true;
     }
