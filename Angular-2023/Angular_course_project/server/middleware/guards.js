122b7360597486fe2e352d35ff640bc0d615af1b
Todor Yadkov <toni_12@abv.bg>
2023-07-16 21:16:05 +0300

    Update guards.js

--- Angular-2023/Angular_course_project/server/middleware/guards.js
+++ Angular-2023/Angular_course_project/server/middleware/guards.js
@@ -39,8 +39,8 @@ function isOwner(req, res, next) {
         next();
 
     } else if (req.user._id == res.locals.preload?.userId?._id) {
-        // Check Comment model
-        // Check if the current user is the owner of the current comment
+        // Check Comment and Order model
+        // Check if the current user is the owner of the current comment or order
         next();
 
     } else {
@@ -76,10 +76,35 @@ function isRoleAdmin(req, res, next) {
     }
 }
 
+function isAllowedTimeToChangeOrders(req, res, next) {
+    if (!req.user) {
+        // Check if the current user is logged in
+        return res.status(401).json({ message: 'Unauthorized', statusCode: 401 });
+
+    } else if (res.locals.preload?.date) {
+        // Check Order model
+        // Check if the current order is create before five minutes
+        const timestampCreated = res.locals.preload?.date; // Timestamp on created date
+        const timestampNow = new Date().getTime(); // Timestamp now
+
+        const timeDifferenceInMilliseconds = Math.abs(timestampCreated - timestampNow); // Calculate difference in milliseconds
+        const allowedTimeIntervalInMilliseconds = 5 * 60 * 1000; // Convert 5 minutes to milliseconds
+
+        if (timeDifferenceInMilliseconds <= allowedTimeIntervalInMilliseconds) {
+            next();
+        } else {
+            return res.status(403).json({ message: 'Forbidden - Time is more than five minutes', statusCode: 403 });
+        }
+    } else {
+        return res.status(403).json({ message: 'Forbidden', statusCode: 403 });
+    }
+}
+
 module.exports = {
     isAuth,
     onlyForGuest,
     isOwner,
     isNotOwner,
     isRoleAdmin,
+    isAllowedTimeToChangeOrders,
 };
