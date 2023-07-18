197ac455d7518ee4a3c3f176b0fb9e942fbc0c07
Todor Yadkov <toni_12@abv.bg>
2023-07-18 13:04:49 +0300

    Update data.service.ts

--- Angular-2023/Angular_course_project/client/gourmet-academy/src/app/core/services/data/data.service.ts
+++ Angular-2023/Angular_course_project/client/gourmet-academy/src/app/core/services/data/data.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { IEnvironments } from 'src/app/models/environment.interfaces';
 import { constants, endpoints } from '../../environments/constants';
 import { IAllRestaurants, IRestaurant } from 'src/app/models/restaurant.interfaces';
-import { IOrderWithProducts } from 'src/app/models/order.interfaces';
+import { IOrder, IOrderWithProducts } from 'src/app/models/order.interfaces';
 import { IComment } from 'src/app/models/comment.interfaces';
 import { IProduct } from 'src/app/models/product.interfaces';
 
@@ -110,15 +110,24 @@ export class DataService {
     const url = constants.hostBackEnd + endpoints.getAllCommentsRestaurant(restaurantId);
     return this.http.get<IComment[]>(url);
   }
-  // Buy product
-  buyFromRestaurant(restaurantId: string, purchaseData: { addressDelivery: string, orders: string[] }): Observable<{ addressDelivery: string, orders: string[] }> {
+  // Buy product (create new order)
+  buyFromRestaurant(restaurantId: string, purchaseData: { addressDelivery: string, orders: string[], date: number }): Observable<{ addressDelivery: string, orders: string[], date: number }> {
     const url = constants.hostBackEnd + endpoints.buyFromRestaurant(restaurantId);
-    return this.http.post<{ addressDelivery: string, orders: string[] }>(url, purchaseData);
+    return this.http.post<{ addressDelivery: string, orders: string[], date: number }>(url, purchaseData);
   }
   // Get user purchased products
   getUserBought(userId: string): Observable<IOrderWithProducts[]> {
     const url = constants.hostBackEnd + endpoints.getUserBought(userId);
     return this.http.get<IOrderWithProducts[]>(url);
   }
-  
+  // Update order
+  updateOrder(orderId: string, orderData: { addressDelivery: string, orders: string[], date: number }): Observable<{ addressDelivery: string, orders: string[], date: number }> {
+    const url = constants.hostBackEnd + endpoints.updateOrder(orderId);
+    return this.http.put<{ addressDelivery: string, orders: string[], date: number }>(url, orderData);
+  }
+  // Delete order
+  deleteOrder(orderId: string): Observable<IOrder> {
+    const url = constants.hostBackEnd + endpoints.deleteOrder(orderId);
+    return this.http.delete<IOrder>(url);
+  }
 }
