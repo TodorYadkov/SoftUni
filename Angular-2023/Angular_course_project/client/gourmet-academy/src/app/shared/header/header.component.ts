cfb2aa574e5aed5d408bd05426d109bb1cb7ba94
Todor Yadkov <toni_12@abv.bg>
2023-07-19 11:37:28 +0300

    Update header.component.ts

--- Angular-2023/Angular_course_project/client/gourmet-academy/src/app/shared/header/header.component.ts
+++ Angular-2023/Angular_course_project/client/gourmet-academy/src/app/shared/header/header.component.ts
@@ -2,10 +2,6 @@ import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { ThemeService } from 'src/app/core/services/theme.service';
 import { Subscription } from 'rxjs';
-import { GetUserIpService } from 'src/app/core/services/users/get-user-ip.service';
-import { DataService } from 'src/app/core/services/data/data.service';
-import { WeatherService } from 'src/app/core/services/data/weather.service';
-import { IForecastDaily, ILocation } from 'src/app/models/wheater.interfaces';
 import { UsersService } from 'src/app/core/services/users/users.service';
 import { ManagerSessionService } from 'src/app/core/services/users/manager-session.service';
 import { Router } from '@angular/router';
@@ -21,80 +17,18 @@ export class HeaderComponent implements OnInit, OnDestroy {
   subscription!: Subscription;
   isUserAdmin!: boolean; // Use to show and hide add-restaurants (Only admin role is allowed to create new restaurant)
 
-  // IF NOT USE WEATHER API - DELETE ALL OF THIS and do NOT forget to delete and services
-  // ipAddressSubscription$!: Subscription;
-  // userIpAddress!: string;
-  // cityObj!: ILocation;
-  // forecastDaily!: IForecastDaily;
-  // isReadyForecast: boolean = false;
-
   constructor(
     @Inject(DOCUMENT) private document: Document, // Use document object to add new class in index.html on html tag
     private themeService: ThemeService,
     private userService: UsersService,
     private managerSession: ManagerSessionService,
-    private router: Router,
-    // private getUserIp: GetUserIpService,
-    // private getData: DataService,
-    // private weatherService: WeatherService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.theme = this.themeService.theme; // Set theme color 
     this.updateThemeColor(); // Set theme color 
-
-    // DO NOT FORGET TO IMPLEMENT WEATHER API IT WORK ONLY TO SET WHERE TO SHOW FOR TEST
-    // this.ipAddressSubscription$ = this.getUserIp  // Get user ip address
-    //   .getIPAddress()
-    //   .subscribe({
-    //     next: (res) => this.userIpAddress = res.ip,
-    //     error: (error) => this.ipAddressSubscription$.unsubscribe(),
-    //     complete: () => {
-
-    //       let accuWeatherAPIKey: string;
-    //       let dataSubscription$: Subscription;
-    //       dataSubscription$ = this.getData.getAPIKeys() // Get wheater API keys
-    //         .subscribe({
-    //           next: (keyObj) => accuWeatherAPIKey = keyObj.accuWeatherApiKey,
-    //           error: (error) => dataSubscription$.unsubscribe(),
-    //           complete: () => {
-
-    //             let ipSubscription$: Subscription;
-    //             ipSubscription$ = this.weatherService.geCityByIP(accuWeatherAPIKey, this.userIpAddress) // Get city by IP - this.userIpAddress
-    //               .subscribe({
-    //                 next: (city) => this.cityObj = city,
-    //                 error: (error) => ipSubscription$.unsubscribe(),
-    //                 complete: () => {
-
-    //                   let forecastSubscription$: Subscription;
-    //                   forecastSubscription$ = this.weatherService.getOneDayForecast(accuWeatherAPIKey, this.cityObj.Key) // Get forecast
-    //                     .subscribe({
-    //                       next: (forecast) => {
-    //                         this.forecastDaily = forecast;
-    //                         this.isReadyForecast = true;
-    //                         console.log(this.forecastDaily) // REMOVE THIS ROW ONLY FOR TEST
-    //                       },
-    //                       error: (error) => forecastSubscription$.unsubscribe(),
-    //                       complete: () => forecastSubscription$.unsubscribe()
-    //                     })
-
-    //                   ipSubscription$.unsubscribe();
-    //                 }
-    //               })
-
-    //             dataSubscription$.unsubscribe();
-    //           }
-    //         })
-    //       this.ipAddressSubscription$.unsubscribe();
-    //     }
-    //   });
-
-
   }
-
-  // getWeatherIcon(iconCode: number): string {
-
-  // }
 
   // check which navigation to display
   updateNav(): boolean {
